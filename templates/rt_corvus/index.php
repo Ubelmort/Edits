cc3c1ddf216d2c42f77169bc04032a7574ab2ed3
Brian Perry <beeperry@gmail.com>
2014-06-05 07:47:13 -0400

    FIX: update .org logo

--- templates/rt_corvus/index.php
+++ templates/rt_corvus/index.php
@@ -78,8 +78,8 @@ if ($gantry->get('loadtransition')) {
 				<?php /** End Drawer **/ endif; ?>
 				<?php /** Begin Top **/ if ($gantry->countModules('top')) : ?>
 				<div id="rt-top">
-					<div class="rt-logo">&nbsp;</div>
 					<div class="rt-container">
+						<div class="rt-logo">&nbsp;</div>
 						<?php echo $gantry->displayModules('top','standard','standard'); ?>
 						<div class="clear"></div>
 					</div>
